94e4a6f0e41b1a1c3bb7623b0f3c86bbbfd312e9
kieran-grant <2357351g@student.gla.ac.uk>
2020-04-11 10:41:07 +0100

    First commit

---
+++ frontend/src/User.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { useQuery, useMutation } from "react-apollo";
+import { gql } from 'apollo-boost';
+
+const QUERY_USERS = gql`
+query {
+  users {
+    id
+    name
+    lastName
+  }
+}
+`;
+
+export function UserInfo() {
+
+    // Polling: rovides near-real-time synchronization with your server
+    // by causing a query to execute periodically at a specified interval
+    const { data, loading, error } = useQuery(QUERY_USERS, { pollInterval: 500 });
+    // should handle loading status
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Unexpected error: {error.message}</p>;
+  
+    return data.users.map(({ id, name, lastName }) => (
+      <div key={ id }>
+        <p>
+          User - { id }: { name } { lastName }
+        </p>
+      </div>
+    ));
+  }
+
+
+const CREATE_USER = gql`
+  mutation createUser ($name: String!, $lastName: String!){
+    createUser (name: $name, lastName: $lastName){
+      id
+      name
+      lastName
+  }
+}
+`;
+
+export function CreateUser() {
+
+    let inputName, inputLastName;
+
+    const [createUser, { data }] = useMutation(CREATE_USER);
+
+    return (
+        <div>
+            <form
+                onSubmit={e => {
+                    e.preventDefault();
+                    createUser({ variables: {
+                        name: inputName.value,
+                        lastName: inputLastName.value
+                    }});
+
+                    inputName.value = '';
+                    inputLastName.value = '';
+                    window.location.reload();
+
+                }}
+
+                style = {{ marginTop: '2em', marginBottom: '2em' }}
+                >
+                
+                <label>First Name :</label>
+                <input
+                    ref={node => {
+                        inputName = node;
+                    }}
+                    style = {{ marginRight: '1em' }}
+                />
+
+                <label>Last Name :</label>
+                <input
+                    ref={node => {
+                        inputLastName = node;
+                    }}
+                    style = {{ marginRight: '1em' }}
+                />
+
+
+                <button type="submit" style={{ cursor: 'pointer' }}>
+                    Add a User
+                </button> 
+            </form>
+        </div>
+    );
+}
